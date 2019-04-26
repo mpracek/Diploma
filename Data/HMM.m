4e2eca1c25f3360191dc41c39699748424a071d9
mpracek <mpracek299@gmail.com>
2019-04-26 11:32:38 +0200

    Malo dopisal

--- Data/HMM.m
+++ Data/HMM.m
@@ -1,17 +1,22 @@
-function[najboljsi_rezultat,varianca,najvec,najmanj] = HMM(podatki,stevilo_stanj)
+function[najboljsi_rezultat,varianca,najvec,najmanj,rezultat] = HMM(podatki,zacetek,stevilo_stanj)
 %V tej datoteki imamo program, s katerim bomo pogledali skriti markovski model. 
-%Stevilo stanj je stevilo, doloceno na podlagi pogleda na podatke.
+%Stevilo stanj je stevilo, doloceno na podlagi pogleda na podatke. V veèini
+%primerov iz naših podatkov je ta vrednost enaka 10.
 %Podatki je vektor podatkov.
 %dolzina je polovica znanih stanj, ker bomo drugo polovico primerjali z dejanskimi vrednostmi.
-A = podatki;
-zaporedje = A.Close;
-dolzina = length(zaporedje)/2;
-opazovanja = A.Close(2:dolzina,:);
+%Rezultat pove, v kakšnem procentu se najbolj verjetno zaporedje ujema z
+%nekim poljubnim
+zaporedje = podatki.Close;
+dolzina = 100;
+stevilo_izhodov = 3;
+opazovanja = podatki.Close(zacetek:zacetek+dolzina,:);
 Prehod = 1/stevilo_stanj .* ones(stevilo_stanj);
-Emit = 1/stevilo_stanj .* ones(stevilo_stanj);
+Emit = 1/stevilo_izhodov .* ones(stevilo_stanj,stevilo_izhodov);
 [prehod,emit] = hmmtrain(opazovanja,Prehod,Emit);
 najboljsi_rezultat = hmmviterbi(opazovanja,prehod, emit);
 razlika = vektor - najboljsi_rezultat;
-najvec = max(razlika)
-najmanj = min(razlika)
-varianca = var(razlika)
+najvec = max(razlika);
+najmanj = min(razlika);
+varianca = var(razlika);
+likelystates = hmmviterbi(opazovanja, Prehod, Emit);
+rezultat = sum(states==likelystates)/1000;
