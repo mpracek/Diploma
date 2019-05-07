89556b32aa8a6816370e30c0e0f732b9fc1a4d97
mpracek <mpracek299@gmail.com>
2019-05-07 09:48:03 +0200

    stanje in HMM

--- Data/HMM.m
+++ Data/HMM.m
@@ -1,4 +1,4 @@
-function[najboljsi_rezultat,varianca,najvec,najmanj,rezultat,standard,vrednosti] = HMM(podatki, zacetek,stevilo_stanj, dolzina)
+function[najboljsi_rezultat,varianca,najvec,najmanj,rezultat,standard,vrednosti] = HMM(podatki, zacetek, dolzina)
 %V tej datoteki imamo program, s katerim bomo pogledali skriti markovski model. 
 %Stevilo stanj je stevilo, doloceno na podlagi pogleda na podatke. V veèini
 %primerov iz naših podatkov je ta vrednost enaka 10.
@@ -6,37 +6,31 @@ function[najboljsi_rezultat,varianca,najvec,najmanj,rezultat,standard,vrednosti]
 %dolzina je polovica znanih stanj, ker bomo drugo polovico primerjali z dejanskimi vrednostmi.
 %Rezultat pove, v kakšnem procentu se najbolj verjetno zaporedje ujema z
 %nekim poljubnim
+%Zaèetek vsaj 11 zaradi definicij
+%število stanj je vedno = 3 (glej stanje.m)
+
 
 %datoteka mora biti navedena v enojnih narekovajih s konènico prek
 %naslednjega ukaza:
 %podatki = importdata(datoteka);
 
-%Izbor podatkov
-opazovanja = podatki.data(zacetek:zacetek+dolzina,4);
-opazovanja2 = podatki.data(zacetek-11:zacetek+dolzina,4);
 
+stevilo_stanj = 3;
+
+%Izbor podatkov
+[stanja] = stanje(zacetek, podatki.data(:,4));
+
+opazovanja = podatki.data(zacetek:zacetek+dolzina,4);
+stanja2 = stanja(zacetek:zacetek+dolzina);
 vektor = podatki.data(zacetek+dolzina:zacetek+dolzina+dolzina-1,4);
 vrednosti = zeros(dolzina,1);
 
-%stanja so lahko pozitiven trend, ni spremembe ali negativen trend.
-%torej rast, padec, stagnacija;
-%za trend pogledamo povpreèno ceno zadnjih 10 dni in primerjamo z danajšnjo
-%èe je rast, bo vrednost stanja(i) = 3, èe je stagnacija bo 2, èe padec = 1
-stanja = zeros(length(opazovanja),1);
-for i = 1:dolzina
-    if sum(opazovanja2(i:i+10))/10 > opazovanja(i)
-        stanja(i) = 3;
-    elseif sum(opazovanja2(i:i+10))/10 < opazovanja(i)
-        stanja(i) = 1;
-    else
-        stanja(i) = 2;
-    end
-end
+
 %Prvi pregled podatkov
 [zaporedje, sredina] = kmeans(opazovanja, stevilo_stanj);
 
 %Trening modela
-[trans,emis]= hmmestimate(zaporedje, randi(3, length(zaporedje),1));
+[trans,emis]= hmmestimate(zaporedje, stanja2);
 %[prehod,emit] = hmmtrain(zaporedje,trans,emis);
 
 %Delni rezultat
@@ -50,11 +44,11 @@ for stevilo = 1:(dolzina)
     end
 end
 
-razlika = abs(vektor - vrednosti);
+razlika = vektor - vrednosti;
 
 %Statisticna "analiza"
-najvec = max(razlika);
-najmanj = min(razlika);
+najvec = max(abs(razlika));
+najmanj = min(abs(razlika));
 varianca = var(razlika);
 standard = std(razlika);
 rezultat = sum(zaporedje==najboljsi_rezultat)/dolzina;
